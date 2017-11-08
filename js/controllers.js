0d3dddef6cae2e56dedbb76d5af1c62996cab331
Manel Charouni <charouni.manel@gmail.com>
2017-11-08 05:55:49 -0500

    testing Firebase service

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,49 @@
-angular.module('ATS-products.controllers', [])
+'use strict'
+angular.module('ATS-products.controllers', ['ATS-products.services'])
 
+    .controller('GetProductsCtrl', function ($scope, productsService) {
+        $scope.products = [];
+        var productsNodeRef = productsService.products;
 
-  .controller('GetProductsCtrl', function () {
+        console.log("===testing===", 'inside products controller');
+        //retrieve products from the firebase reference using the productsService
+        var productsListPromise = new Promise(function (resolve, reject) {
+            // query the products DB
+            productsNodeRef.once('value').then(function (productsSnapshot) {
+                $scope.products=productsSnapshot.val();
+                if ($scope.products.length !== 0 ) {
+                    resolve(products);
+                }
+                else {
+                    var reason = new Error('couldn\'t load products ');
+                    reject(reason);
+                }
+               /* productsSnapshot.forEach(function (child) {
 
-     console.log("testing",'inside products controller');
+                    var product = child.val();
+                    // push product object to array
+                    products.push(product);
 
+                    if (products.length === productsSnapshot.length) {
+                        resolve(products);
+                    }
+                    else {
+                        var reason = new Error('couldn\'t load products ');
+                        reject(reason);
+                    }
+
+                })*/
+
+            })
+        });
+        //calling  promise
+        console.log("calling products promise...");
+        productsListPromise
+            .then(function (productsList) {
+                console.log((productsList));
+        })
+            .catch(function (error) {
+                console.error(error.message);
+
+            })
     });
--- js/services.js
+++ js/services.js
@@ -1,9 +1,15 @@
 angular.module('ATS-products.services', [])
 
+    .factory('productsService',
+        function () {
+            console.log("getting DB reference");
+            var root = firebase.database().ref();
+            var service = {
+                root: root,
+                products: root.child('products')
+            };
+            return service;
+        }
 
-
-.factory('productsService', function() {
-
-
-  });
+ );
 
