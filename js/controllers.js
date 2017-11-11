2b8dfae30a675f35b01a1d3089e2bc1195f56e4a
Manel Charouni <charouni.manel@gmail.com>
2017-11-10 19:59:09 -0500

    pushing  version 2.0

--- js/controllers.js
+++ js/controllers.js
@@ -1,47 +1,65 @@
 (function () {
-    'use strict';
-    //TODO split the services from the controller code (to the productsService code)
-    //TODO add a $http service consuming the ATS webservice and filling the frebase DB with the incoming data
-    //TODO add the pagination functionality
+        'use strict';
+        //TODO split the services from the controller code (to the productsService code)
+        //TODO add the pagination functionality
 
-    angular.module('ATS-products.controllers', ['ATS-products.services'])
+        angular.module('ATS-products.controllers', ['ATS-products.services'])
 
-        .controller('getProductsCtrl', getProductsCtrl);
-    getProductsCtrl.$inject = ['$scope', '$firebaseArray', 'productsService'];
-    function getProductsCtrl($scope, $firebaseArray, productsService) {
+            .controller('getProductsCtrl', getProductsCtrl);
+        getProductsCtrl.$inject = ['$http', '$firebaseArray', 'productsService'];
+        function getProductsCtrl($http, $firebaseArray, productsService) {
 
-        var vm = this;
-        vm.loading = true;
-        vm.categories = [];
-        $scope.productKeys = [];
-        vm.pages = 0;
-        $scope.sortKey = ""; //(not working)
-        //TODO fix this function
-        //order products in a random way using one of the keys
-        vm.randomize = function () {
-            console.log("randomizing in action...");
-            $scope.sortKey = $scope.productKeys [Math.floor(Math.random() * $scope.productKeys.length)];
-            console.log($scope.sortKey);
-        };
-        var ref = firebase.database().ref().child('products');
-        // download the data into a local object
+            var vm = this;
+            vm.loading = true;
+            vm.products = [];
+            vm.categories = [];
+            vm.productKeys = [];
+            vm.pages = 0;
+            vm.sortKey = ""; //(not working)
 
-        $firebaseArray(ref).$loaded().then(function (data) {
-            vm.products = data;
-            vm.pages = data.length / 20;
-            vm.loading = false;
-            //save products keys (used by randomize() )
-            $scope.productKeys.push(Object.keys(data[0]));
-            console.log($scope.productKeys);
-            //retreive distinct categories to be used as filetring options
-            data.forEach(function (product) {
-                //save products categories used to filter rendered data
-                if ((vm.categories.indexOf(product.category) < 0)) {
-                    vm.categories.push(product.category);
+            //order products in a random way using one of the keys
+            vm.randomize = function () {
+                console.log("randomizing in action...");
+                vm.productKeys = ['basePrice', 'brand', 'category', 'delivery', 'details', 'imageUrl', 'productMaterial', 'productName', 'reviews'];
+                var ind = Math.floor(Math.random() * vm.productKeys.length);
+                vm.sortKey = vm.productKeys [ind];
+                console.log("sorting using the attribute:", vm.sortKey);
+            };
+
+            var ref = firebase.database().ref().child('products');
+            // this will be the synchronized products array (allowing 3 way data binding) any change in this array is vizualised in UI in realtime
+            vm.products = $firebaseArray(ref);
+            //check the content of the Firebase array
+            vm.products.$loaded().then(function (data) {
+                    //if DB is empty
+                    if (data.length === 0) {
+                        console.log("DB is empty..");
+                        $http.get("http://internal.ats-digital.com:3066/api/products")
+                            .then(function (products) {
+                                    console.log("adding products..");
+                                    console.log(products.data);
+                                    vm.products.$add(products.data);
+                                    console.log("added products..", vm.products);
+                                },
+                                function (error) {
+                                    console.error("Error adding data...")
+
+                                });
+                    }
+                    //else : DB is filled with products data
+                    vm.loading = false;
+                    data.forEach(function (product) {
+                        //retreive distinct categories to be used as filetring options
+                        product.forEach(function (p) {
+                            if ((vm.categories.indexOf(p.category) < 0)) {
+                                vm.categories.push(p.category);
+                            }
+                        })
+                    });
+                    console.log(vm.categories);
                 }
-            });
-            console.log(vm.categories);
-        });
-
-    }
-})();
+                , function (error) {
+                    console.log("error loading products...")
+                });
+        }
+    })();
